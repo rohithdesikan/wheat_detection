d98d15af8987afb435a538f1a54c4ea11ff6ac62
Rohith Desikan <rohithdesikan@gmail.com>
2020-09-22 21:57:22 -0700

    Added a requirements file and environment (pywheat)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='wheat_detection',
     packages=find_packages(),
     version='0.1.0',
     description='This project aims to detectt wheat in plant images',
